9ababc7acc19613dd8e7fd7e98d749a44d2ffb80
Gokhan9 <gcancomert@hotmail.com>
2026-06-13 22:56:17 +0300

    walletservice ve walletrepo yerleri değiştirdim.

--- internal/core/ports/wallet_ports.go
+++ internal/core/ports/wallet_ports.go
@@ -5,16 +5,7 @@ import (
 	"go-hexagonal/internal/core/domain"
 )
 
-// ?WalletRepository-Driven Port(İkincil)
-// APP'in veriyi nasıl saklayacağının kontratı
-// DB(postgres,redis vb..) bu interface'i implement eder.
-type WalletRepository interface {
-	Create(ctx context.Context, wallet *domain.Wallet) error
-	GetByID(ctx context.Context, id string) (*domain.Wallet, error)
-	Update(ctx context.Context, wallet *domain.Wallet) error
-}
-
-// ?WalletService - Driving Port (Birincil Liman)
+// ?WalletService - Driver Port - Primary Port (Birincil)
 // Uygulamanın sunduğu iş yeteneklerinin kontratıdır.
 // Handler'lar (HTTP API) bu interface'i çağıracak.
 type WalletService interface {
@@ -22,4 +13,13 @@ type WalletService interface {
 	GetWallet(ctx context.Context, id string) (*domain.Wallet, error)
 	Deposit(ctx context.Context, walletID string, amount int64) error
 	Withdraw(ctx context.Context, walletID string, amount int64) error
+}
+
+// ?WalletRepository - Driven Port - Secondary Port (İkincil)
+// APP'in veriyi nasıl saklayacağının kontratı
+// DB(postgres,redis vb..) bu interface'i implement eder.
+type WalletRepository interface {
+	Create(ctx context.Context, wallet *domain.Wallet) error
+	GetByID(ctx context.Context, id string) (*domain.Wallet, error)
+	Update(ctx context.Context, wallet *domain.Wallet) error
 }
