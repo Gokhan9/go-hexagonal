80f220781903d691e358d0e38797c6e38605db5d
Gokhan9 <gcancomert@hotmail.com>
2026-06-13 21:47:53 +0300

    main dosyası oluşturuldu ve inject işlemleri yapıldı, routelar eklendi ve http server start ayarlaması yapıldı.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"go-hexagonal/internal/adapters/handler"
+	"go-hexagonal/internal/adapters/repository"
+	services "go-hexagonal/internal/core/service"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// 1. Repository (Driven Adapter - Altyapı) başlatma işlemi
+	repo := repository.NewMemoryWalletRepository()
+
+	// 2. Service (Core İş Mantığı) başlatma işlemi ve "repo" inject
+	walletService := services.NewWalletService(repo)
+
+	// 3. Handler (Driving Adapter - API Katmanı) başlatma işlemi ve "service" inject
+	walletHandler := handler.NewWalletHandler(walletService)
+
+	// 4. HTTP ServeMux
+	mux := http.NewServeMux()
+
+	// 5. API Route
+	mux.HandleFunc("POST /wallets", walletHandler.Create)
+	mux.HandleFunc("GET /wallets/{id}", walletHandler.GetByID)
+	mux.HandleFunc("POST /wallets/{id}/deposit", walletHandler.Deposit)
+	mux.HandleFunc("POST /wallets/{id}/withdraw", walletHandler.Withdraw)
+
+	// 6. HTTP Server Starting
+	log.Println("Sunucu:8080 Portunda çalışıyor......")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("Sunucu başlatılamadı: %v", err)
+	}
+}
