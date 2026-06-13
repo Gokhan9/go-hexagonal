0c5155b9bf70bd299604e422c23d414d96c083be
Gokhan9 <gcancomert@hotmail.com>
2026-06-13 14:29:25 +0300

    dependency injection, ID üretmek için uuid paketi ve iş kuralları (deposit, withdraw) eklendi.

---
+++ internal/core/services/wallet_service.go
@@ -0,0 +1,67 @@
+package services
+
+import (
+	"context"
+	"go-hexagonal/internal/core/domain"
+	"go-hexagonal/internal/core/ports"
+
+	"github.com/google/uuid"
+)
+
+// dependency inject
+type walletService struct {
+	repo ports.WalletRepository
+}
+
+func NewWalletService(repo ports.WalletRepository) ports.WalletService {
+	return &walletService{
+		repo: repo,
+	}
+}
+
+func (s *walletService) CreateWallet(ctx context.Context, owner, currency string) (*domain.Wallet, error) {
+	// factory methodu ile yeni bir wallet oluşturmak..
+	wallet := &domain.Wallet{
+		ID:       uuid.NewString(),
+		Owner:    owner,
+		Balance:  0,
+		Currency: currency,
+	}
+
+	if err := s.repo.Create(ctx, wallet); err != nil {
+		return nil, err
+	}
+
+	return wallet, nil
+}
+
+func (s *walletService) GetWallet(ctx context.Context, id string) (*domain.Wallet, error) {
+	return s.repo.GetByID(ctx, id)
+}
+
+func (s *walletService) Deposit(ctx context.Context, walletID string, amont int64) error {
+	wallet, err := s.repo.GetByID(ctx, walletID)
+	if err != nil {
+		return err
+	}
+
+	// İş modelini domain modeli üzerindeki metodla işletmek.
+	if err := wallet.Deposit(amont); err != nil {
+		return err
+	}
+
+	return s.repo.Update(ctx, wallet)
+}
+
+func (s *walletService) Withdraw(ctx context.Context, walletID string, amount int64) error {
+	wallet, err := s.repo.GetByID(ctx, walletID)
+	if err != nil {
+		return err
+	}
+
+	if err := wallet.Withdraw(amount); err != nil {
+		return err
+	}
+
+	return s.repo.Update(ctx, wallet)
+}
