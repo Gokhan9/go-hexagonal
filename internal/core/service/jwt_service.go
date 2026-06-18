da22d6527fd24b4931160ba82ca011f56dccb1ea
Gokhan9 <gcancomert@hotmail.com>
2026-06-19 02:08:18 +0300

    yorum satırları eklendi.

--- internal/core/service/jwt_service.go
+++ internal/core/service/jwt_service.go
@@ -6,18 +6,36 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+/*
+1. Token üretme (muhtemelen başka fonksiyonda)
+-Kullanıcı login olur
+-UserClaims oluşturulur
+-JWT imzalanır
+
+2. Token doğrulama
+-Secret key ile imza kontrol edilir
+-Expiration kontrol edilir
+*/
+
+// JWT Üretmek ve Doğrulamak.
 type JWTService struct {
-	secretKey []byte
-	issuer    string
-	expiry    time.Duration
+	secretKey []byte        // Tokenları imzalamak ve doğrulamak için unique key. JWT kütüphanesi "[]byte" ister.
+	issuer    string        // Token'ı üreten bilgisi(örn:wallet-api).
+	expiry    time.Duration // Token süresi
 }
 
+// JWT içine koyulacak payload(veri) kısmı.
 type UserClaims struct {
-	UserID   string `json:"user_id"`
-	Username string `json:"username"`
-	jwt.RegisteredClaims
+	UserID               string `json:"user_id"`
+	Username             string `json:"username"`
+	jwt.RegisteredClaims        // Embedded. Standart JWT alanlarını ekler. (iss-issuer, exp-expiration(bitiş zamanı), iat-issued at(oluşturulma zamanı), sub(subject))
 }
 
+/*
+- "secret" string alıyor, byte'a çevirip saklıyor.
+- "issuer" sabit olarak, "wallet-api"
+- "expirt" dışarıdan geliyor.
+*/
 func NewJWTService(secret string, expiry time.Duration) *JWTService {
 	return &JWTService{
 		secretKey: []byte(secret),
