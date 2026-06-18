4c25f595c9d3363fc58518ee1d278f1626d4352d
Gokhan9 <gcancomert@hotmail.com>
2026-06-19 01:45:56 +0300

    auth errorslar eklendi.

--- internal/core/domain/errors.go
+++ internal/core/domain/errors.go
@@ -7,4 +7,9 @@ var (
 	ErrorInsufficientFunds    = errors.New("Insufficient funds in wallet.")      // Cüzdanda Yetersiz Bakiye
 	ErrorInvalidAmount        = errors.New("Amount must be greater than 0.....") // Geçersiz Miktar
 	ErrConcurrentModification = errors.New("Eşzamanlı değişiklik hatası..")
+
+	// Auth Errors
+	ErrorUserAlreadyExists  = errors.New("Username is already taken")    // ilgili username'in önceden alınmış olması
+	ErrorInvalidCredentials = errors.New("Invalid username or password") // login sırasında username/e-posta eşleşmemesi
+	ErrorUnauthorized       = errors.New("Unauthorized access")          // kimlik doğrulama(authentication) başarısız(401).
 )
