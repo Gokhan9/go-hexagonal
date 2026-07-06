b4ce3ae448d411ef7da7df4cdd376e664a2a696d
Gokhan9 <gcancomert@hotmail.com>
2026-07-06 21:35:30 +0300

    feat(service): Transfer ve GetBalance iş mantığı implemente edildi

--- backend/internal/core/service/wallet_service.go
+++ backend/internal/core/service/wallet_service.go
@@ -53,7 +53,6 @@ func (s *walletService) GetWallet(ctx context.Context, userID, id string) (*doma
 	return wallet, nil
 }
 
-// ! 2. ADIM : WalletService, "Deposit ve Withdraw" Güncellemesi
 func (s *walletService) Deposit(ctx context.Context, idempotencyKey string, walletID string, userID string, transactionID string, amount int64) error {
 
 	txContext, err := s.repo.BeginTx(ctx)
@@ -228,4 +227,79 @@ func (s *walletService) Withdraw(ctx context.Context, idempotencyKey string, wal
 func (s *walletService) GetTransactions(ctx context.Context, walletID string) ([]*domain.Transaction, error) {
 
 	return s.repo.GetTransactionsByWalletID(ctx, walletID)
+}
+
+func (s *walletService) Transfer(ctx context.Context, idempotencyKey, fromWalletID, toWalletID, ownerID string, amount int64) error {
+
+	// guard clause
+	if fromWalletID == toWalletID {
+		return domain.ErrorSelfTransfer
+	}
+
+	if amount <= 0 {
+		return domain.ErrorInvalidAmount
+	}
+
+	// Transaction Start
+	// NOT: BeginTx, transaction'ı ctx içerisine gömer (context-based propagation)
+	ctx, err := s.repo.BeginTx(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Transaction tamamlanmazsa "panic&hata" rollback'i dön. Database tutarlılığını sağlar.
+	defer s.repo.Rollback(ctx)
+
+	// * Business Logics
+
+	// Gönderen(withdraw) wallet'ı çek
+	fromWallet, err := s.repo.GetByID(ctx, fromWalletID)
+	if err != nil {
+		return err
+	}
+
+	// Gönderen(withdraw) wallet'ın sahibi mi kontrolü
+	if fromWallet.OwnerID != ownerID {
+		return domain.ErrorUnauthorized
+	}
+
+	// Alıcı(deposit) wallet'ı çek.
+	toWallet, err := s.repo.GetByID(ctx, toWalletID)
+	if err != nil {
+		return err
+	}
+
+	// balance check
+	if fromWallet.Balance < amount {
+		return domain.ErrorInsufficientFunds
+	}
+
+	// İşlemleri Gerçekleştir.
+
+	// withdraw(gönderen)
+	fromWallet.Balance -= amount
+	if err := s.repo.Update(ctx, fromWallet); err != nil {
+		return err
+	}
+
+	// deposit(alıcı)
+	toWallet.Balance += amount
+	if err := s.repo.Update(ctx, toWallet); err != nil {
+		return err
+	}
+
+	// commit (işlemlerin başarılı olması durumunda.)
+	return s.repo.Commit(ctx)
+}
+
+// GetByID ile wallet'a ait balance(bakiye) bilgisini döner.
+func (s *walletService) GetBalance(ctx context.Context, walletID string) (int64, error) {
+
+	wallet, err := s.repo.GetByID(ctx, walletID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return wallet.Balance, nil
 }
