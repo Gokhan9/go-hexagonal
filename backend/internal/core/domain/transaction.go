b93cae53f7bd27a8ff1fae086a1af3dd95de5c4f
Gokhan9 <gcancomert@hotmail.com>
2026-07-11 13:18:43 +0300

    transaction'da yapılacak işlemler için pending, completed ve failed gibi yeni sabitler ekledim ve struct içerisinde status olarak tanımladım.

--- backend/internal/core/domain/transaction.go
+++ backend/internal/core/domain/transaction.go
@@ -2,14 +2,19 @@ package domain
 
 import "time"
 
-/*
-Transaction, wallet içerisindeki tüm para hareketlerini (deposit, withdraw, transfer, fee vb) sistemde kayıt altına alan domain entity’dir. */
-type TransactionType string
+type TransactionType string   // Transaction, wallet içerisindeki tüm para hareketlerini (deposit, withdraw, transfer, fee vb) sistemde kayıt altına alan domain entity’dir.
+type TransactionStatus string // Transaction İşlemi başladığında PENDING, başarı durumunda COMPLETED, hata durumunda FAILED statüsüne geçirmek.
 
-// sabitler(enums)
+// SABİTLER(enums)
 const (
 	Deposit  TransactionType = "DEPOSIT"
 	Withdraw TransactionType = "WITHDRAW"
+)
+
+const (
+	StatusPending   TransactionStatus = "PENDING"
+	StatusCompleted TransactionStatus = "COMPLETED"
+	StatusFailed    TransactionStatus = "FAILED"
 )
 
 type Transaction struct {
@@ -17,5 +22,6 @@ type Transaction struct {
 	WalletID  string
 	Amount    int64 // ! "int64" yapıldı. finansal sistemlerde yuvarlama hatalarından kaçınmak. (kuruş/cent bazında) kullanmalıyız..
 	Type      TransactionType
+	Status    TransactionStatus
 	CreatedAt time.Time
 }
