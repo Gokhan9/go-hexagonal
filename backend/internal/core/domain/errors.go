96c0f109c9ec9129d36a01a67fbb2782e2ee11a6
Gokhan9 <gcancomert@hotmail.com>
2026-07-19 21:48:58 +0300

    domain içinde ki error mesajı düzeltildi ve yorum satırları eklendi.

--- backend/internal/core/domain/errors.go
+++ backend/internal/core/domain/errors.go
@@ -4,19 +4,19 @@ import "errors"
 
 // Uygulama genelinde aşağıdaki hata tanımlarını kullanacağız..
 var (
-	ErrorInsufficientFunds    = errors.New("Insufficient funds in wallet.")      // Cüzdanda Yetersiz Bakiye
-	ErrorInvalidAmount        = errors.New("Amount must be greater than 0.....") // Geçersiz Miktar
-	ErrConcurrentModification = errors.New("Eşzamanlı değişiklik hatası..")
+	ErrorInsufficientFunds    = errors.New("Insufficient funds in wallet.")  // Cüzdanda Yetersiz Bakiye
+	ErrorInvalidAmount        = errors.New("Amount must be greater than 0.") // Geçersiz Miktar
+	ErrConcurrentModification = errors.New("Eşzamanlı değişiklik hatası.")
 
 	// Auth Errors
-	ErrorUserAlreadyExists  = errors.New("Username is already taken")    // ilgili username'in önceden alınmış olması
-	ErrorInvalidCredentials = errors.New("Invalid username or password") // login sırasında username/e-posta eşleşmemesi
-	ErrorUnauthorized       = errors.New("Unauthorized access")          // kimlik doğrulama(authentication) başarısız(401).
+	ErrorUserAlreadyExists  = errors.New("Username is already taken")    // İlgili username'in önceden alınmış olması
+	ErrorInvalidCredentials = errors.New("Invalid username or password") // Login sırasında username/e-posta eşleşmemesi
+	ErrorUnauthorized       = errors.New("Unauthorized access")          // Kimlik doğrulama(authentication) başarısız(401).
 
 	// wallet error
 	ErrorWalletNotFound = errors.New("Wallet not found..")
 
-	ErrorSelfTransfer        = errors.New("Self transfer is not allowed..") // kendi cüzdanına transfer yapılamaz.
-	ErrorWalletAlreadyClosed = errors.New("Wallet is already closed.")
-	ErrorWalletNotEmptied    = errors.New("Wallet must be empty to close")
+	ErrorSelfTransfer        = errors.New("Self transfer is not allowed..") // Kendi cüzdanına transfer yapılamaz.
+	ErrorWalletAlreadyClosed = errors.New("Wallet is already closed.")      // Wallet kapatma
+	ErrorWalletNotEmptied    = errors.New("Wallet must be empty to close")  // Wallet kapatmak için boş olmalı.
 )
